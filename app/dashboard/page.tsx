36374da9d0012a3f9b9dd7a1a99f55e3c76c6dcf
Ying Hsiu Lai <susan8213@gmail.com>
2025-05-04 10:20:11 +0800

    demo of Streaming, and React.Suspense component
    - to optimize ux while data fetching, fetch smaller component to not await others.
    - Suspense to deal with ui while awaiting / loading

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,9 @@
-import { Card } from '@/app/ui/dashboard/cards';
+import CardWrapper from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
+import { Suspense } from 'react';
+import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
 
 // 錯誤的寫法：
 // 使用箭頭函式定義組件，雖然語法上沒有問題，但這種寫法可能導致 React DevTools 將組件名稱顯示為匿名函式，
@@ -22,9 +23,6 @@ import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/app/lib/data
 // 使用命名函式的方式定義組件，這樣可以提供穩定的函式名稱，避免 hydration error，
 // 並且更符合 Next.js 的最佳實踐，讓框架更容易識別這是一個頁面組件。 
 export default async function Dashboard() {
-    const revenue = await fetchRevenue();
-    const latestInvoices = await fetchLatestInvoices();
-    const { totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers } = await fetchCardData();
 
   return (
     <main>
@@ -32,18 +30,17 @@ export default async function Dashboard() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Card title="Collected" value={totalPaidInvoices} type="collected" />
-        <Card title="Pending" value={totalPendingInvoices} type="pending" />
-        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-        <Card
-          title="Total Customers"
-          value={numberOfCustomers}
-          type="customers"
-        />
+        <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue}  />
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback={<RevenueChartSkeleton />}>
+            <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+            <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
